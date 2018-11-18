809ab0677f513b7b0b7e9f2b95fbce428835959b
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-18 23:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return console.log(string.toUpperCase)
+  console.log(string.toUpperCase)
 }
 
 function logWhisper(string) {
- return console.log(string.toLowerCase)
+ console.log(string.toLowerCase)
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) {
-    string = "I can't hear you"
+    string = "I can't hear you!"
   } else if (string === string.toUpperCase) {
-    string = "YES INDEED"
+    string = "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     string = "I love you, too."
   }
