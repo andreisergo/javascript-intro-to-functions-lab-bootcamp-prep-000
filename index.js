cd89e796818770048fbe6abb8c50630b22065582
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-18 23:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    string = "I can't hear you!"
+     return "I can't hear you!"
   } else if (string === string.toUpperCase()) {
     string = "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
